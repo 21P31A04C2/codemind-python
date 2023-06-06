fd76e7493e592262ef73432d3afcf6f2a7faf637
21P31A04C2 <99953138+21P31A04C2@users.noreply.github.com>
2023-06-06 14:19:21 +0530

    def prime(n):     c=0     for i in range(2,n):         if n%i==0:             c=c+1     if c==0:         return True     return False a =int(input()) c=[] for i in range(1,a+1):     if a%i==0:         if prime(i):             pass         else:             c.append(i) print(len(c)+1)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,14 +1,17 @@
-def np(x):
-    
-    for i in range(2,x):
-        if x%i==0:
-            return True
-    else:
-        return False
-x=int(input())
-c=0
-for i in range(1,x+1):
-    if x%i==0:
-        if np(i):
+def prime(n):
+    c=0
+    for i in range(2,n):
+        if n%i==0:
             c=c+1
-print(c+1)
+    if c==0:
+        return True
+    return False
+a =int(input())
+c=[]
+for i in range(1,a+1):
+    if a%i==0:
+        if prime(i):
+            pass
+        else:
+            c.append(i)
+print(len(c)+1)
